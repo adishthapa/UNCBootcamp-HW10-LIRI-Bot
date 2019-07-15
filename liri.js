1d93fdc63746a261ce76ddeec01b4750f34b45b0
adishthapa <adishthapa@outlook.com>
2019-07-15 17:34:21 -0400

    Added switch statements

--- liri.js
+++ liri.js
@@ -12,3 +12,33 @@ var bandsintown = keys.bandsintown;
 var omdb = keys.omdb;
 // Spotify API Key
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+
+switch(command) {
+    case "concert-this":
+        console.log("A");
+        break;
+    case "spotify-this-song":
+        console.log("B");
+        break;
+    case "movie-this":
+        console.log("C");
+        break;
+    case "do-what-it-says":
+        console.log("D");
+        break;
+    default:
+        console.log("+-----------------------------------------------------+")
+        console.log("|                                                     |")
+        console.log("| The given command is not recognized.                |");
+        console.log("|                                                     |")
+        console.log("| Please try one of the following commands instead:   |");
+        console.log("|                                                     |")
+        console.log("| node liri.js concert-this '<artist/band name here>' |");
+        console.log("| node liri.js spotify-this-song '<song name here>'   |");
+        console.log("| node liri.js movie-this '<movie name here>'         |");
+        console.log("| node liri.js do-what-it-says                        |");
+        console.log("|                                                     |")
+        console.log("+-----------------------------------------------------+")
+};
