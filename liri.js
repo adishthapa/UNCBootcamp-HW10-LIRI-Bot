0f136882a62802d7836f265cad9c5a84f8369896
adishthapa <adishthapa@outlook.com>
2019-07-16 00:44:19 -0400

    Finished implementing functions for spotify-this-song

--- liri.js
+++ liri.js
@@ -18,10 +18,8 @@ var command = process.argv[2];
 function concertThis(artist) {
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + bandsintown)
     .then(function (response) {
-        // var were = response.data
-        // console.log(were);
         if (response.data != "\n{warn=Not found}\n" && response.data.length > 0) {
-            console.log("-------------------------------------------------------\n")
+            console.log("---------------------------------------------------------------------------\n")
             for (var i = 0; i < response.data.length; i++) {
                 console.log("Venue Name: " + response.data[i].venue.name + "\n");
                 console.log("Country: " + response.data[i].venue.country);
@@ -32,23 +30,54 @@ function concertThis(artist) {
                 concertDate = moment(concertDate).format("MM/DD/YYYY")
                 console.log("Date: " + concertDate);
                 if (i === response.data.length - 1) {
-                    console.log("\n-------------------------------------------------------") 
+                    console.log("\n---------------------------------------------------------------------------") 
                 } else {
-                    console.log("\n-------------------------------------------------------\n")
+                    console.log("\n---------------------------------------------------------------------------\n")
                 }
-                
             }
         } else {
-            console.log("-------------------------------------------------------\n")
+            console.log("---------------------------------------------------------------------------\n")
             console.log("Sorry, no upcomming events found for '" + artist +"'");
-            console.log("\n-------------------------------------------------------")
+            console.log("\n---------------------------------------------------------------------------")
         }
         
     }).catch(function (error) {
-        console.log("-------------------------------------------------------\n")
+        console.log("---------------------------------------------------------------------------\n")
         console.log("Sorry, an error has occured. Please try again later.");
-        console.log("\n-------------------------------------------------------")
+        console.log("\n---------------------------------------------------------------------------")
     })
+};
+
+function spotifyThisSong(song) {
+
+    spotify.search({ type: 'track', query: song, limit: 5 })
+    .then(function(response) {
+        console.log("---------------------------------------------------------------------------\n")
+        for (var i = 0; i < response.tracks.items.length; i++) {
+            console.log("Result #" + (i + 1) + "\n");
+            console.log("Song: " + response.tracks.items[i].name)
+            var artists = "Artist: ";
+            for (var j = 0; j < response.tracks.items[i].artists.length; j++ ) {
+                if (j === response.tracks.items[i].artists.length - 1) {
+                    artists += response.tracks.items[i].artists[j].name;
+                } else {
+                    artists += response.tracks.items[i].artists[j].name + " & ";
+                }
+            }
+            console.log(artists);
+            console.log("Album: " + response.tracks.items[i].album.name);
+            console.log("Preview: " + response.tracks.items[0].external_urls.spotify)
+            if (i === response.tracks.items.length - 1) {
+                console.log("\n---------------------------------------------------------------------------") 
+            } else {
+                console.log("\n---------------------------------------------------------------------------\n")
+            }
+        };
+    }).catch(function(err) {
+        console.log("---------------------------------------------------------------------------\n")
+        console.log("Sorry, an error has occured. Please try again later.");
+        console.log("\n---------------------------------------------------------------------------")
+    });
 };
 
 switch(command) {
@@ -60,41 +89,48 @@ switch(command) {
             } else {
                 artist += process.argv[i] + " ";
             }
-            
         }
         if (artist) {
             concertThis(artist);
         } else {
-            console.log("+-----------------------------------------------------+")
-            console.log("|                                                     |")
-            console.log("| Please make sure to enter the name of an Artist or  |")
-            console.log("| a Band after the command.                           |")
-            console.log("|                                                     |")
-            console.log("| Example:                                            |")
-            console.log("|                                                     |")
-            console.log("| node liri.js concert-this '<artist/band name here>' |");
-            console.log("|                                                     |")
-            console.log("+-----------------------------------------------------+")
+            console.log("+-------------------------------------------------------------------------+")
+            console.log("|                                                                         |")
+            console.log("| Please make sure to enter the name of an Artist or a Band after the     |")
+            console.log("| command.                                                                |")
+            console.log("|                                                                         |")
+            console.log("| Example:                                                                |")
+            console.log("|                                                                         |")
+            console.log("| node liri.js concert-this '<artist/band name here>'                     |");
+            console.log("|                                                                         |")
+            console.log("+-------------------------------------------------------------------------+")
         }
         break;
     case "spotify-this-song":
         var song = "";
         for (var i = 3; i < process.argv.length; i++) {
-            song += process.argv[i];
+            if (i === process.argv.length - 1) {
+                song += process.argv[i];
+            } else {
+                song += process.argv[i] + " ";
+            }
         }
         if (song) {
-            console.log(song);
+            spotifyThisSong(song);
         } else {
-            console.log("+-----------------------------------------------------+")
-            console.log("|                                                     |")
-            console.log("| Please make sure to enter the name of a Song after  |")
-            console.log("| the command.                                        |")
-            console.log("|                                                     |")
-            console.log("| Example:                                            |")
-            console.log("|                                                     |")
-            console.log("| node liri.js spotify-this-song '<song name here>'   |");
-            console.log("|                                                     |")
-            console.log("+-----------------------------------------------------+")
+            // The direction says to show default song of "The Sign" by Ace of Base if nothing is provided like this:
+            // spotifyThisSong("The Sign Ace of Base");
+            // However, I think the following output looks much better for User Friendliness:
+
+            console.log("+-------------------------------------------------------------------------+")
+            console.log("|                                                                         |")
+            console.log("| Please make sure to enter the name of a Song after the command.         |")
+            console.log("|                                                                         |")
+            console.log("| Example:                                                                |")
+            console.log("|                                                                         |")
+            console.log("| node liri.js spotify-this-song '<song name here>'                       |");
+            console.log("|                                                                         |")
+            console.log("+-------------------------------------------------------------------------+")
+
         }
         break;
     case "movie-this":
@@ -105,31 +141,30 @@ switch(command) {
         if (movie) {
             console.log(movie);
         } else {
-            console.log("+-----------------------------------------------------+")
-            console.log("|                                                     |")
-            console.log("| Please make sure to enter the name of a Movie after |")
-            console.log("| the command.                                        |")
-            console.log("|                                                     |")
-            console.log("| Example:                                            |")
-            console.log("|                                                     |")
-            console.log("| node liri.js movie-this '<movie name here>'         |");
-            console.log("|                                                     |")
-            console.log("+-----------------------------------------------------+")
+            console.log("+-------------------------------------------------------------------------+")
+            console.log("|                                                                         |")
+            console.log("| Please make sure to enter the name of a Movie after the command.        |")
+            console.log("|                                                                         |")
+            console.log("| Example:                                                                |")
+            console.log("|                                                                         |")
+            console.log("| node liri.js movie-this '<movie name here>'                             |");
+            console.log("|                                                                         |")
+            console.log("+-------------------------------------------------------------------------+")
         }
         break;
     case "do-what-it-says":
         break;
     default:
-        console.log("+-----------------------------------------------------+")
-        console.log("|                                                     |")
-        console.log("| The given command is not recognized.                |");
-        console.log("|                                                     |")
-        console.log("| Please try one of the following commands instead:   |");
-        console.log("|                                                     |")
-        console.log("| node liri.js concert-this '<artist/band name here>' |");
-        console.log("| node liri.js spotify-this-song '<song name here>'   |");
-        console.log("| node liri.js movie-this '<movie name here>'         |");
-        console.log("| node liri.js do-what-it-says                        |");
-        console.log("|                                                     |")
-        console.log("+-----------------------------------------------------+")
+        console.log("+-------------------------------------------------------------------------+")
+        console.log("|                                                                         |")
+        console.log("| The given command is not recognized.                                    |");
+        console.log("|                                                                         |")
+        console.log("| Please try one of the following commands instead:                       |");
+        console.log("|                                                                         |")
+        console.log("| node liri.js concert-this '<artist/band name here>'                     |");
+        console.log("| node liri.js spotify-this-song '<song name here>'                       |");
+        console.log("| node liri.js movie-this '<movie name here>'                             |");
+        console.log("| node liri.js do-what-it-says                                            |");
+        console.log("|                                                                         |")
+        console.log("+-------------------------------------------------------------------------+")
 };
